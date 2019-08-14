6c6d6a13f5770e55cf1ec19e790b74267df814ad
Maximiliano Osorio <maxiosorio@gmail.com>
2019-08-13 22:58:47 -0400

    Release v1.3.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="wings",
-    version="1.3.3",
+    version="1.3.4",
     description="Interact with a WINGS instance",
     long_description=README,
     long_description_content_type="text/markdown",
