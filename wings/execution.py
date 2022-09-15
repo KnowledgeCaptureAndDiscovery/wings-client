fe47efe7fde6ba8c9be9711622e8941247c3c023
Varun Ratnakar <varunratnakar@gmail.com>
2022-09-15 16:05:04 +0530

    Fixes for viewing execution

--- wings/execution.py
+++ wings/execution.py
@@ -16,16 +16,21 @@ class Execution(object):
 
         resp = self.api_client.session.get(self.api_client.get_request_url() + 'executions/getRunListSimple',
                                            params=params)
-        return self.api_client.check_request(resp)
+        return resp.json()
+
+    def get_execution_url(self, execution_id):
+        return self.api_client.get_export_url() + 'executions/' + execution_id + '.owl#' + execution_id
 
     def get_run_details(self, execution_id):
-        postdata = {'run_id': execution_id}
+        exurl = self.get_execution_url(execution_id)
+        postdata = {'run_id': exurl}
         resp = self.api_client.session.post(self.api_client.get_request_url() + 'executions/getRunDetails',
                                             data=postdata)
-        return self.api_client.check_request(resp)
+        return resp.json()
 
     def delete_run(self, execution_id):
-        json_data = json.dumps({"id": execution_id})
+        exurl = self.get_execution_url(execution_id)
+        json_data = json.dumps({"id": exurl})
         postdata = {'json': json_data}
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
@@ -36,6 +41,7 @@ class Execution(object):
         return self.api_client.check_request(resp)
 
     def publish(self, execution_id):
-        postdata = {'run_id': execution_id}
+        exurl = self.get_execution_url(execution_id)
+        postdata = {'run_id': exurl}        
         resp = self.api_client.session.post(self.api_client.get_request_url() + 'executions/publishRun', data=postdata)
         return self.api_client.check_request(resp)
